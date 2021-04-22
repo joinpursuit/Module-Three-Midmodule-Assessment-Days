b5c30e24cf158b927c716977d54199cbe69e55fb
Talia Pichardo <taliapichardo@gmail.com>
2021-04-22 13:10:02 -0400

    products test passing

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import formatPrice from '../helpers/formatPrice'
 
 const Product = (props) =>{
     return(
         <div>
             <h3>{props.data.name}</h3>
-            <p>Price:${props.data.price}</p>
+            <p>Price: {formatPrice(props.data.price)}</p>
             <button>Add To Cart</button>
             <br/>
             <img src={props.data.img}></img>
