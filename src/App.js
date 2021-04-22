661cbda2f20918c725b05ea4823206d28bd867ad
Jasleen Villamil <jasleenvillamil@gmail.com>
2021-04-22 14:39:42 -0400

    first commit struggling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import React, { Component } from 'react'
+import Cart from "./Cart"
+import Checkout from "./Checkout"
+import Items from "./Items"
+import productData from "./data/productData"
+
+
+export default class App extends Component {
+  render() {
+    const items = {productData}
+    const  items  = this.props
+    const addedItems = addedItems
+
+
+    return (
+      <div class="App">
+        <div>
+          <div className="leftSide" />
+          <h1>My Garage Sale</h1>
+          <Items Items={items} />
+        </div>
+        <div className="rightSide">
+          <Cart
+            addedItems={addedItems}
+            subtotal={subtotal}
+            tax={tax}
+            total={total} />
+
+          <Checkout
+            total={total} />
+
+
+        </div>
+      </div>
+    )
+  }
+}
+
+
 import "./App.css";
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
-};
 
-export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import productData from "./data/productData"
 
 ReactDOM.render(
   <React.StrictMode>
