40a7652d4d0cc08a7b85b1dbc673b1d737be13b9
Jon Narine <jonnicwolf@gmail.com>
2021-04-22 13:29:14 -0400

    productList populated and seen

---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Item(prop) {
+    return (
+        <div>
+            <h2>{prop.name}</h2>
+            <div>{prop.price}</div>
+            <button>Add To Cart</button>
+            <img
+                alt='pic'
+                src={prop.img}/>
+            <div>{prop.description}</div>
+        </div>
+    )
+}
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import productData from '../data/productData.js'
+import Item from './Item.js'
 
-
-export default function ProductList() {
-    console.log('productData',productData)
+export default function ProductList(prop) {
+    // const productData = prop.productData
+    // console.log('productData', productData)
+    
     return (
-        <div>
-            <h1>item</h1>
-            <div>price</div>
-            <button>Add to Cart</button>
-            <img
-                alt='pic'></img>
-            <div>item description</div>
-        </div>
+        <ul>
+            {productData.map((item) => {
+                return (
+                    <Item
+                        key={item.id}
+                        name={item.name}
+                        price={item.price}
+                        description={item.description}
+                        img={item.img}
+                    />
+               )
+           })} 
+        </ul>
     )
 }
