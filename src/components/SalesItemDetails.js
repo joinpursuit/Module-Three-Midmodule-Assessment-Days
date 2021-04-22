aabf7f76f491ae0d0a138cf090d3c1a61a328b9c
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-04-22 16:08:09 -0400

    rendering data from product data

--- src/components/SalesItemDetails.js
+++ src/components/SalesItemDetails.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 const SalesItemDetails = ({item, itemUp}) => {
     const { name, price, description, img } = item;
+
     return (
         <div>
             <h3>{name}</h3>
-            Price: ${price.toFixed(2)}
+            <p>Price: ${price.toFixed(2)}</p>
             <button onClick={() => itemUp(item)}>Add To Cart</button>
             <img src={img} />
-            <p>description</p>
+            <p>{description}</p>
         </div>
     )
 }
