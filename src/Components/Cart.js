6b21eadaffe7ab39ea82589c3276024a80b90c31
imran nazir <inazir2016@gmail.com>
2021-04-27 14:49:21 -0400

    tested

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,29 +1,27 @@
 import React from 'react'
 import './../App.css'
  
+const Cart = ({cart, total, subtotal, tax}) => {
 
-  
-        const Cart = ({cart,total, tax, subtotal})=>{
-            let cartList =cart.map(product =>{
-                return (
-                    <li key ={product.id}> {product.name}: {product.price.toFixed(2)}
-                 </li>
-                )
-            })
-
+    let cartList = cart.map(product => {
         return (
+            <li key={product.id}> {product.name}: ${product.price.toFixed(2)} </li>
+        )
+    })
+
+    return (
             <div className ="cart">
                 <h2>Cart</h2>
                 <ul>
-                    {cartList}
+                    {cartList} 
                 </ul>
-                <h4>Subtal: ${subtotal.toFixed(2)}</h4>
-                <h4> Tax: ${tax.toFixed(2)}</h4>
+                <h4>Subtotal: ${subtotal.toFixed(2)}</h4>
+                <h4>Tax: ${tax.toFixed(2)}</h4>
                 <h4>Total: ${total.toFixed(2)}</h4>
 
             </div>
             
-        )
-    }
+    )
+}
 
 export default Cart
