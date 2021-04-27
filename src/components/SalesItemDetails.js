8aa8a53c68499d250fa64e9577cd7ee8afe96671
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-04-27 16:21:43 -0400

    Mid-mod assessment re-submit

--- src/components/SalesItemDetails.js
+++ src/components/SalesItemDetails.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import "./SalesItemDetails.css"
 
 const SalesItemDetails = ({item, itemUp}) => {
     const { name, price, description, img } = item;
 
     return (
-        <div>
+        <section className="Products">
             <h3>{name}</h3>
             <p>Price: ${price.toFixed(2)}</p>
             <button onClick={() => itemUp(item)}>Add To Cart</button>
             <img src={img} />
             <p>{description}</p>
-        </div>
+        </section>
     )
 }
 
