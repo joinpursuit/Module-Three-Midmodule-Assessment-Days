79f0edbe9469704b6d722e31dc5c3b4fa1c75bcb
Du Min Chen <dm.chen0113@gmail.com>
2021-05-24 23:34:40 -0400

    Add missing hook to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import { useState } from "react"
 import productData from "./data/productData"
 import Products from "./components/Products"
 import Cart from "./components/Cart"
 import Checkout from "./components/Checkout"
 import "./App.css"
 
+export default function App() {
+  const [ cartItems, setCartItems ] = useState([]);
+  const [ purchase, setPurchase ] = useState({ subtotal: 0, tax: 0, total: 0 });
+
+  const addToCart = (item) => {
+    const newCartItems = [...cartItems, item];
+    const subtotal = newCartItems.reduce((sum, item) => sum + item.price, 0);
+    const tax = subtotal * 0.05;
+    const total = subtotal + tax;
+    setCartItems(newCartItems);
+    setPurchase({ subtotal, tax, total });
+  }
+
+  return (
+    <div className="App">
+      <div className="left">
+        <h1>My Garage Sale</h1>
+        <Products
+          productsList={productData}
+          addToCart={addToCart}
+        />
+      </div>
+      <div className="right">
+        <Cart
+          cartItemsList={cartItems}
+          subtotal={purchase.subtotal}
+          tax={purchase.tax}
+          total={purchase.total}
+        />
+        <Checkout
+          total={purchase.total}
+        />
+      </div>
+    </div>
+  )
+}
+/*
 export default class App extends Component {
   constructor(){
     super()
@@ -42,3 +80,4 @@ export default class App extends Component {
     )
   }
 }
+*/
